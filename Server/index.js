46aaf5f08ea8d777a706319b31701ad67d9ec25c
dvpalm <dv.palmiero@gmail.com>
2022-02-20 12:04:50 +0100

    Added server filter

--- Server/index.js
+++ Server/index.js
@@ -12,7 +12,11 @@ let news = [];
 let urlAPI = 'https://newsapi.org/v2/everything?q=apple&from=2022-02-16&to=2022-02-16&sortBy=popularity&apiKey=eed23c4b4a74423abababde8cc78f44a';
 
 fetch(urlAPI).then(response => response.json()).then((data) => {
-    news = data.articles;
+     data.articles.forEach(function(article){
+            if (article.author != null && article.author.split(' ').length -1 >=1 && article.author.split(' ').length -1 < 3) {
+                news.push(article);
+            }
+    });
 });
 
 app.get('/news', (request, response) => {
